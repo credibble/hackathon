032e256d2558d1c785f45126ada63ff571bfef9a
devarogundade <devarogundade@gmail.com>
2025-08-13 11:42:24 +0100

    update waitlist port

--- waitlist-server/index.ts
+++ waitlist-server/index.ts
@@ -206,7 +206,7 @@ app.get("/verify", async (req: Request, res: Response) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
